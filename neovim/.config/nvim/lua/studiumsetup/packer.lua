4f8edb5814eb6eaed996bc52841173ecedd6647c
Philipp Klausen <philipp.klausen@web.de>
2023-05-06 17:00:35 +0200

    Neovim: Fixup basic setup

--- neovim/.config/nvim/lua/studiumsetup/packer.lua
+++ neovim/.config/nvim/lua/studiumsetup/packer.lua
@@ -50,5 +50,5 @@ return require('packer').startup(function(use)
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
-    use 'm4xshen/autoclose.nvim'
+    use ('m4xshen/autoclose.nvim')
 end)
