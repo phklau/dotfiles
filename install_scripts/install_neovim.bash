7604dc6b09684c27dd31adb1dee61f7beeb07016
Mensch <Mensch@abakus.local>
2023-05-07 15:42:05 +0200

    Fixup install script neovim

--- install_scripts/install_neovim.bash
+++ install_scripts/install_neovim.bash
@@ -3,6 +3,15 @@
 #set -x
 set -e
 
-sudo apt install neovim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+while true; do
+    read -p "Do you have nvim installed? (y/n) " yn
+
+    case $yn in
+        [yYjJ] ) break;;
+        [nN] ) echo "Please install nvim first ..."
+            exit;;
+    esac
+done
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+source ../neovim/.config/nvim/lua/studiumsetup/packer.lua
 echo "Run :PackerSync to install Plugins"
